84e5cfc67a891cabcd2c096cd37614831fade2cc
unknown <38482481+cstimler@users.noreply.github.com>
2018-04-24 22:06:47 -0700

    Mostly starter code with a few early layout changes in activity_detail.xml

---
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,12 @@
+package com.udacity.sandwichclub.utils;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+String json = ""
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        return null;
+    }
+}
